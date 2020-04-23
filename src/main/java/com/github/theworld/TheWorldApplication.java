bff181da956a7b8efa61e4bde5c87426628332cd
antonio craveiro <pe_1992@hotmail.com>
2020-04-23 19:08:22 +0200

    Spring-boot project

---
+++ src/main/java/com/github/theworld/TheWorldApplication.java
@@ -0,0 +1,13 @@
+package com.github.theworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/theworld/TheWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.theworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
